b7278de73f93cf42ad5c71e0ef425c4d1fd2d2f3
edukowicz <26.ellie.dukowicz@carolinaday.org>
2025-04-01 13:24:03 +0000

    message

--- Main.java
+++ Main.java
@@ -14,5 +14,19 @@ class Main {
       System.out.println(Arrays.toString(ray1));
       System.out.println(SelSorter.sel_sort_Entry(ray1));
       System.out.println(Arrays.toString(ray1)); 
+
+      int[] arr = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+      for (int i =0; i < arr.length-1; i++) {
+        for(int j =i; j < arr.length; j++) {
+          if(arr[j]<arr[i]) {
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+          }
+        }
+      }
+    for(int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + ", ");
+    }
   }
 }
